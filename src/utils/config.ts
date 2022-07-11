65d7042fec9771e8dadb4267e29112e2b4b04db7
Slava Cherkesov <chrtyaka@gmail.com>
2022-07-11 08:46:49 +0400

    feature(multiprojects): Add utils for find project, team in config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,34 +1,16 @@
 import { GitlabUserNotFoundError } from '../errors/gitlab';
-import { ConfigProjectNotFoundError } from '../errors/config';
+import {
+  ConfigProjectNotFoundError,
+  ConfigProjectTeamNotFound,
+} from '../errors/config';
 import {
   ConfigTeamMember,
   ConfigDiscord,
   ConfigProjectItem,
   ConfigProjects,
+  ConfigTeam,
 } from '../types/config';
 import { getConfig } from '../app-config';
-
-export const findUser = (
-  value: string | number,
-  arg: keyof ConfigTeamMember = 'gitlabId',
-  projectId: number,
-): ConfigTeamMember | GitlabUserNotFoundError | ConfigProjectNotFoundError => {
-  const project = findProjectById(projectId);
-
-  if (project instanceof ConfigProjectNotFoundError) {
-    return project;
-  }
-
-  const { team } = project;
-
-  const user = team.find((item) => item[arg] === value.toString());
-
-  if (!user) {
-    return new GitlabUserNotFoundError(value);
-  }
-
-  return user;
-};
 
 function findProjectById(
   id: number,
@@ -53,6 +35,45 @@ function findProjectById(
 
   return project;
 }
+
+export function findTeamByProjectId(
+  projectId: number,
+): ConfigTeam | ConfigProjectNotFoundError | ConfigProjectTeamNotFound {
+  const project = findProjectById(projectId);
+
+  if (project instanceof ConfigProjectNotFoundError) {
+    return project;
+  }
+
+  if (!project.team || project.team.length === 0) {
+    return new ConfigProjectTeamNotFound(projectId);
+  }
+
+  return project.team;
+}
+
+export const findUser = (
+  value: string | number,
+  projectId: number,
+  arg: keyof ConfigTeamMember = 'gitlabId',
+): ConfigTeamMember | GitlabUserNotFoundError | ConfigProjectNotFoundError => {
+  const team = findTeamByProjectId(projectId);
+
+  if (
+    team instanceof ConfigProjectNotFoundError ||
+    team instanceof ConfigProjectTeamNotFound
+  ) {
+    return team;
+  }
+
+  const user = team.find((item) => item[arg] === value.toString());
+
+  if (!user) {
+    return new GitlabUserNotFoundError(value);
+  }
+
+  return user;
+};
 
 export function findDiscordSettingsByProject(
   projectId: number,
