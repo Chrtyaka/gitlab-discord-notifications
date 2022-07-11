f5c5a5d6faa61dccb93fce817c6921d385068066
Slava Cherkesov <chrtyaka@gmail.com>
2022-07-11 08:43:13 +0400

    feature(multiprojects): Add error class for handling config team not found

--- src/errors/config.ts
+++ src/errors/config.ts
@@ -1,8 +1,15 @@
 class ConfigProjectNotFoundError extends Error {
-  constructor(projectId: number | string) {
+  constructor(projectId: number) {
     super(`Project not found in config: ${projectId}`);
     this.name = 'ConfigProjectNotFoundError';
   }
 }
 
-export { ConfigProjectNotFoundError };
+class ConfigProjectTeamNotFound extends Error {
+  constructor(projectId: number) {
+    super(`Team not found in project: ${projectId}`);
+    this.name = 'ConfigProjectNotFoundError';
+  }
+}
+
+export { ConfigProjectNotFoundError, ConfigProjectTeamNotFound };
